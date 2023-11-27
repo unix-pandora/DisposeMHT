d4dcd0a12746c67144e4359c16e5070b36374dff
unix-pandora <wvwoov@163.com>
2023-11-27 01:50:00 -0500

    second commit

--- src/api/extract_values.py
+++ src/api/extract_values.py
@@ -9,7 +9,7 @@ def extract_hrefs_from_html(html_file_path, class_name):
     with open(html_file_path, 'r', encoding='utf-8') as f:  
         soup = BeautifulSoup(f.read(), 'html.parser')  
   
-    # 找到所有类名为"ss-image"的标签  
+    # 找到所有类名为 class_name 的标签  
     tags = soup.find_all(class_=class_name)  
   
     # 提取每个标签的href属性值，存入列表  
